a98e395b7cbd33585ac1e93f6912dde88d1acf4a
郝仕兢 <haoshijing@corp.netease.com>
2018-05-03 11:08:38 +0800

    数据库用户名密码取修改

--- job/src/main/java/com/youzi/balance/job/SyncDataJob.java
+++ job/src/main/java/com/youzi/balance/job/SyncDataJob.java
@@ -39,7 +39,7 @@ public class SyncDataJob {
     private SystemTotalMapper systemTotalMapper;
 
 
-    @Scheduled(cron = "0 0/1 * * * ?")
+    @Scheduled(cron = "0 0/2 * * * ?")
     public void sync() {
         try {
             log.info("开始同步");
