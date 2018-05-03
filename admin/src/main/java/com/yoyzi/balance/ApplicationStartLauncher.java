93bf2065d7b3e2d1d870c5c6a35f8d6868a60a6c
郝仕兢 <haoshijing@corp.netease.com>
2018-05-03 09:16:35 +0800

    修改admin为war包

---
+++ admin/src/main/java/com/yoyzi/balance/ApplicationStartLauncher.java
@@ -0,0 +1,21 @@
+package com.yoyzi.balance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+@EnableAutoConfiguration
+@ImportResource({"classpath:application-context.xml"})
+public class ApplicationStartLauncher extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApplicationStartLauncher.class);
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ApplicationStartLauncher.class).web(true).run(args);
+    }
+}
